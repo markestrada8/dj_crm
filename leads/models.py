3976738cdf0c7efd7803dde4085c5f89b688ba23
Mark Estrada <mark.estrada.dev>
2023-08-13 02:34:55 -0500

    update

---
+++ leads/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Agent(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.user.email}'
+
+# class Lead(models.Model):
+#     SOURCE_CHOICES = (
+#         ('Youtube', 'Youtube'),
+#         ('Google', 'Google'),
+#         ('Newsletter', 'Newsletter')
+#     )
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     age = models.IntegerField(default=0)
+
+#     phoned = models.BooleanField(default=False)
+#     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+#     profile_picture = models.ImageField(blank=True, null=True)
+#     special_files = models.FileField(blank=True, null=True)
+
+
